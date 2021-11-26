12d2c541c0760c7b56b2998067298aa508fe6f75
María Gabriela Delgado <g-abby091@outlook.com>
2021-11-26 15:36:54 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
-app.use(express.static(__dirname+'/dist/observatorioLA'));
-app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/dist/observatorioLA/index.html'));
+app.use(express.static(__dirname+'/dist/Dashboard'));
+app.get('/*',function(req,res){
+  res.sendFile(path.join(__dirname+'/dist/Dashboard/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
